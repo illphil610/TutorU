d4a96af937734368a8fc9680257726310b8cf9c3
Philip <pcappelli901@gmail.com>
2018-01-29 10:37:46 -0500

    test commit for phil

--- app/src/main/java/com/newwesterndev/tutoru/MainActivity.java
+++ app/src/main/java/com/newwesterndev/tutoru/MainActivity.java
@@ -6,10 +6,13 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     String test = "testing push";
+    String testForPhil = "YAY FOR LYNDA VIDEOS";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Test comment for the haters
     }
 }
