cf9b927ddfdaa28e80f6aaa714db9698831229dc
Philip <pcappelli901@gmail.com>
2018-01-29 10:55:19 -0500

    another test comment

--- app/src/main/java/com/newwesterndev/tutoru/MainActivity.java
+++ app/src/main/java/com/newwesterndev/tutoru/MainActivity.java
@@ -15,6 +15,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //Test comment for the haters
-        //Another test
+        //Another test comment
     }
 }
