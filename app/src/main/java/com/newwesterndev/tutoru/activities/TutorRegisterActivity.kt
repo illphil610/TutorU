cd3dd7539a4724c985f355f41e26c4bdb81bf442
Derek Witteck <drwitteck@gmail.com>
2018-04-17 22:11:00 -0400

    Created custom dialogs to handle subject and course selection when a tutor registers for the app.

--- app/src/main/java/com/newwesterndev/tutoru/activities/TutorRegisterActivity.kt
+++ app/src/main/java/com/newwesterndev/tutoru/activities/TutorRegisterActivity.kt
@@ -1,23 +1,21 @@
 package com.newwesterndev.tutoru.activities
 
+import android.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
-import android.widget.Spinner
+import android.widget.Toast
 import com.newwesterndev.tutoru.R
-import org.jetbrains.anko.find
 
 class TutorRegisterActivity : AppCompatActivity() {
 
     private lateinit var email: EditText
     private lateinit var password: EditText
     private lateinit var confirmPassword: EditText
-    private lateinit var courseSpinner: Spinner
-    private lateinit var subjectSpinner: Spinner
+    private lateinit var subjectCourseButton: Button
     private lateinit var submitButton: Button
     private lateinit var cancelButton: Button
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,11 +24,26 @@ class TutorRegisterActivity : AppCompatActivity() {
         email = findViewById(R.id.edit_text_tutor_reg_email)
         password = findViewById(R.id.edit_text_tutor_reg_password)
         confirmPassword = findViewById(R.id.edit_text_tutor_reg_password)
-        courseSpinner = findViewById(R.id.spinner_course)
-        subjectSpinner = findViewById(R.id.spinner_subject)
+        subjectCourseButton = findViewById(R.id.button_subject_course)
         submitButton = findViewById(R.id.button_tutor_reg_submit)
         cancelButton = findViewById(R.id.button_tutor_reg_cancel)
 
+        subjectCourseButton.setOnClickListener { showSubjectCourseSelectionAlertDialog() }
+    }
 
+    private fun showSubjectCourseSelectionAlertDialog() {
+        val dialogBuilder = AlertDialog.Builder(this)
+        dialogBuilder.setTitle("Select Subjects")
+        dialogBuilder.setIcon(R.mipmap.ic_books)
+
+        dialogBuilder.setPositiveButton("Now Select Courses") {dialog, which ->
+            Toast.makeText(this, "Subjects Selected", Toast.LENGTH_LONG).show()
+        }
+
+        dialogBuilder.setNegativeButton("Cancel") {dialog, which ->
+            Toast.makeText(this, "Canceled", Toast.LENGTH_LONG).show()
+        }
+
+        dialogBuilder.create().show()
     }
 }
